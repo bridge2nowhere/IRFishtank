4693b17aee42949c9e86a5a3f25eafab5b3dfd30
bridge2nowhere <38335227+bridge2nowhere@users.noreply.github.com>
2019-08-09 21:20:37 -0700

    Update asta20_test1.ino

--- asta20_test1.ino
+++ asta20_test1.ino
@@ -12,12 +12,40 @@ RTC_DS3231 rtc;
 #define TODBUTTON 4
 #define OVERRIDEBUTTON 5
 
-const unsigned long irCh1 = 0xFFC03FUL;
-const unsigned long irCh2 = 0xFFE01FUL;
-const unsigned long irCh3 = 0xFF20DFUL;
-const unsigned long irCh4 = 0xFF609FUL;
-const unsigned long irPlus = 0xFF708FUL;
-const unsigned long irMinus = 0xFF906FUL;
+/*Ch1 0xFFC03F
+Ch1 + 0xFF30CF
+Ch1 - 0xFF906F
+
+Ch2 0xFFE01F
+Ch2 + 0xFF50AF
+Ch2 -  0xFFA857
+
+Ch3. 0xFF20DF
+Ch3 +  0xFFB04F
+Ch3 -  0xFF6897
+
+Ch4. 0xFF609F
+Ch4 + 0xFF708F
+Ch4 - 0xFF28D7
+ */
+
+const unsigned long irCh1 =  0xFFC03FUL;
+const unsigned long irCh1P = 0xFF30CFUL;
+const unsigned long irCh1M = 0xFF906FUL;
+
+const unsigned long irCh2 =  0xFFE01FUL;
+const unsigned long irCh2P = 0xFF50AFUL;
+const unsigned long irCh2M = 0xFFA857UL;
+
+
+const unsigned long irCh3 =  0xFF20DFUL;
+const unsigned long irCh3P = 0xFFB04FUL;
+const unsigned long irCh3M = 0xFF6897UL;
+
+
+const unsigned long irCh4 =  0xFF609FUL;
+const unsigned long irCh4P = 0xFF708FUL;
+const unsigned long irCh4M = 0xFF28D7UL;
 
 int sendDelay = 1000;
 
@@ -45,29 +73,29 @@ void loop()
 //test code goes between the two bars
 /////////////////////////////////////////////////////
   //set ch2
-  irsend.sendNEC(0xFFE01F,32);
+  irsend.sendNEC(irCh2,32);
   delay(sendDelay);
   
   //test + and -
-  irsend.sendNEC(0xFF708F,32);
+  irsend.sendNEC(irCh2P,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF708F,32);
+  irsend.sendNEC(irCh2P,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF708F,32);
+  irsend.sendNEC(irCh2P,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF708F,32);
+  irsend.sendNEC(irCh2P,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF708F,32);
+  irsend.sendNEC(irCh2P,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF906,32);
+  irsend.sendNEC(irCh2M,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF906,32);
+  irsend.sendNEC(irCh2M,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF906,32);
+  irsend.sendNEC(irCh2M,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF906,32);
+  irsend.sendNEC(irCh2M,32);
   delay(sendDelay);
-  irsend.sendNEC(0xFF906,32);
+  irsend.sendNEC(irCh2M,32);
   delay(sendDelay);
 
 ////////////////////////////////////////////////////
@@ -88,6 +116,3 @@ void loop()
 //  oled.println(chan4);
   
 }  
-  
-  
-  
